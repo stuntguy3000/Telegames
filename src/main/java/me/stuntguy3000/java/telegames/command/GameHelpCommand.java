0e9aa7a9342eee60fc54b89adec4b51e6f1a08da
Luke Anderson <stuntguy3000@gmail.com>
2015-12-01 00:45:10 +1030

    Missed a command

---
+++ src/main/java/me/stuntguy3000/java/telegames/command/GameHelpCommand.java
@@ -0,0 +1,30 @@
+package me.stuntguy3000.java.telegames.command;
+
+import me.stuntguy3000.java.telegames.Telegames;
+import me.stuntguy3000.java.telegames.object.Command;
+import me.stuntguy3000.java.telegames.object.Game;
+import pro.zackpollard.telegrambot.api.chat.Chat;
+import pro.zackpollard.telegrambot.api.event.chat.message.CommandMessageReceivedEvent;
+
+// @author Luke Anderson | stuntguy3000
+public class GameHelpCommand extends Command {
+    public GameHelpCommand() {
+        super(Telegames.getInstance(), "gamehelp", "/gamehelp <game> View specific help information for a game");
+    }
+
+    public void processCommand(CommandMessageReceivedEvent event) {
+        Chat chat = event.getChat();
+
+        if (event.getArgs().length < 1) {
+            respond(chat, "Correct Syntax: /gamehelp <game>");
+        } else {
+            Game game = getInstance().getGameHandler().getGame(event.getArgs()[0]);
+
+            if (game != null) {
+                respond(chat, "Help for " + game.getGameName() + ":\n" + game.getGameHelp());
+            } else {
+                respond(chat, "Game \"" + event.getArgs()[0] + "\" could not be found.\nUse /gamelist for help.");
+            }
+        }
+    }
+}
