781dacf846dce8f73e185653f8e19d464a4f81a2
Luke Anderson <stuntguy3000@gmail.com>
2015-12-03 22:15:37 +1030

    Patch minor error

--- src/main/java/me/stuntguy3000/java/telegames/util/StringUtil.java
+++ src/main/java/me/stuntguy3000/java/telegames/util/StringUtil.java
@@ -2,8 +2,8 @@ package me.stuntguy3000.java.telegames.util;
 
 // @author Luke Anderson | stuntguy3000
 public class StringUtil {
-    public static String isPlural(Number amount) {
-        return amount.toString().equalsIgnoreCase("1") ? "" : "s";
+    public static String isPlural(int amount) {
+        return amount == 1 ? "" : "s";
     }
 }
     
