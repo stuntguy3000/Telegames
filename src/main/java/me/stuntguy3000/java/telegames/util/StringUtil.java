c04a5a5140957d03985ab31343144898bd568970
Luke Anderson <stuntguy3000@gmail.com>
2015-12-03 22:15:55 +1030

    Remove unneeded code

--- src/main/java/me/stuntguy3000/java/telegames/util/StringUtil.java
+++
@@ -1,9 +0,0 @@
-package me.stuntguy3000.java.telegames.util;
-
-// @author Luke Anderson | stuntguy3000
-public class StringUtil {
-    public static String isPlural(int amount) {
-        return amount == 1 ? "" : "s";
-    }
-}
-    
