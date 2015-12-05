f85ec1f92beda0458813ac1c893515b1d46833e8
Luke Anderson <stuntguy3000@gmail.com>
2015-12-05 13:15:18 +1030

    Fix issue regarding resources

--- src/main/java/me/stuntguy3000/java/telegames/game/CardsAgainstHumanity.java
+++ src/main/java/me/stuntguy3000/java/telegames/game/CardsAgainstHumanity.java
@@ -205,7 +205,7 @@ public class CardsAgainstHumanity extends Game {
 
     // Load Card Packs from Jar resources
     public void loadPacks() {
-        InputStream is = getClass().getResourceAsStream("cardsagainsthumanity/v3.cards");
+        InputStream is = getClass().getResourceAsStream("cah.v3.cards");
         BufferedReader reader = new BufferedReader(new InputStreamReader(is));
         CAHCardPack cahCardPack = new CAHCardPack();
         String packLine;
