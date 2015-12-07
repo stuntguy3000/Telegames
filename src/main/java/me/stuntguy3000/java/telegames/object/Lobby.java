38a2c41dae6a686320d6f3338ddd9edd7fe603d2
Luke Anderson <stuntguy3000@gmail.com>
2015-12-07 10:56:36 +1030

    Debug

--- src/main/java/me/stuntguy3000/java/telegames/object/Lobby.java
+++ src/main/java/me/stuntguy3000/java/telegames/object/Lobby.java
@@ -103,7 +103,19 @@ public class Lobby {
 
         if (currentGame == null) {
             if (message.startsWith("▶️ ")) {
-                message.replace("▶️ ", "");
+                int indexToRemove = 0;
+
+                for (char c : message.toCharArray()) {
+                    if ((c >= 'a' && c <= 'z') ||
+                            (c >= 'A' && c <= 'Z') ||
+                            (c >= '0' && c <= '9')) {
+                        break;
+                    }
+
+                    indexToRemove++;
+                }
+
+                message.substring(indexToRemove);
 
                 event.getChat().sendMessage(message, TelegramHook.getBot());
 
