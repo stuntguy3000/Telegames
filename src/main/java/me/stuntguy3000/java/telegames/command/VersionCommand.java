fefa24b50f17c0f11a963ee36879bfe132dddca9
Luke Anderson <stuntguy3000@gmail.com>
2015-12-17 21:30:07 +1030

    Modify version command to include debug info, and support markdown

--- src/main/java/me/stuntguy3000/java/telegames/command/VersionCommand.java
+++ src/main/java/me/stuntguy3000/java/telegames/command/VersionCommand.java
@@ -4,6 +4,8 @@ import me.stuntguy3000.java.telegames.Telegames;
 import me.stuntguy3000.java.telegames.object.Command;
 import me.stuntguy3000.java.telegames.util.TelegramEmoji;
 import pro.zackpollard.telegrambot.api.chat.Chat;
+import pro.zackpollard.telegrambot.api.chat.message.send.ParseMode;
+import pro.zackpollard.telegrambot.api.chat.message.send.SendableTextMessage;
 import pro.zackpollard.telegrambot.api.event.chat.message.CommandMessageReceivedEvent;
 
 // @author Luke Anderson | stuntguy3000
@@ -15,6 +17,8 @@ public class VersionCommand extends Command {
     public void processCommand(CommandMessageReceivedEvent event) {
         Chat chat = event.getChat();
 
-        respond(chat, TelegramEmoji.GHOST.getText() + " Telegames by @stuntguy3000\n\nSource: https://github.com/stuntguy3000/telegames\nCurrent build " + Telegames.BUILD + ".");
+        respond(chat, SendableTextMessage.builder().message(TelegramEmoji.GHOST.getText() + "*Telegames " + (Telegames.DEV_MODE ? "Dev Mode" : "") + "by @stuntguy3000*\n\n" +
+                "*Source:* https://github.com/stuntguy3000/telegames\n" +
+                "*Current version:* " + Telegames.BUILD).parseMode(ParseMode.MARKDOWN).disableWebPagePreview(true).build());
     }
 }
