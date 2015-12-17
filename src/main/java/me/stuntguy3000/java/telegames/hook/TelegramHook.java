887502f8ad49d71664a10445f064c9edbba97d95
Luke Anderson <stuntguy3000@gmail.com>
2015-12-17 21:35:29 +1030

    Issue resolved.

--- src/main/java/me/stuntguy3000/java/telegames/hook/TelegramHook.java
+++ src/main/java/me/stuntguy3000/java/telegames/hook/TelegramHook.java
@@ -58,7 +58,6 @@ public class TelegramHook implements Listener {
         allGames.stream().filter(Game.class::isAssignableFrom).forEach(clazz -> {
             try {
                 Game game = (Game) clazz.newInstance();
-                LogHandler.debug(clazz.getSimpleName() + " | " + game.isDevModeOnly() + " | " + Telegames.DEV_MODE);
                 if (game.isDevModeOnly() && !Telegames.DEV_MODE) {
                     return;
                 }
