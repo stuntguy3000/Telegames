cdabe70fafa1a3a0295cd208bc66cac5623462da
Luke Anderson <stuntguy3000@gmail.com>
2015-12-17 23:26:45 +1030

    Version command typo fix

--- src/main/java/me/stuntguy3000/java/telegames/command/VersionCommand.java
+++ src/main/java/me/stuntguy3000/java/telegames/command/VersionCommand.java
@@ -17,7 +17,7 @@ public class VersionCommand extends Command {
     public void processCommand(CommandMessageReceivedEvent event) {
         Chat chat = event.getChat();
 
-        respond(chat, SendableTextMessage.builder().message(TelegramEmoji.GHOST.getText() + "*Telegames" + (Telegames.DEV_MODE ? " Dev Mode " : " ") + "by @stuntguy3000*\n\n" +
+        respond(chat, SendableTextMessage.builder().message(TelegramEmoji.GHOST.getText() + " *Telegames" + (Telegames.DEV_MODE ? " Dev Mode " : " ") + "by* @stuntguy3000\n\n" +
                 "*Source:* https://github.com/stuntguy3000/telegames\n" +
                 "*Current version:* " + Telegames.BUILD + "\n\n" +
                 "*Stay up-to-date with new features!*\n Join https://telegram.me/telegames").parseMode(ParseMode.MARKDOWN).disableWebPagePreview(true).build());
