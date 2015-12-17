595feea1d97743c221931e378ae17fff9bfa9e6c
Luke Anderson <stuntguy3000@gmail.com>
2015-12-18 00:20:31 +1030

    Improve console commands

--- src/main/java/me/stuntguy3000/java/telegames/object/LobbyTimer.java
+++ src/main/java/me/stuntguy3000/java/telegames/object/LobbyTimer.java
@@ -2,6 +2,7 @@ package me.stuntguy3000.java.telegames.object;
 
 import me.stuntguy3000.java.telegames.Telegames;
 import me.stuntguy3000.java.telegames.handler.LobbyHandler;
+import me.stuntguy3000.java.telegames.handler.LogHandler;
 
 import java.util.ArrayList;
 import java.util.TimerTask;
@@ -14,6 +15,7 @@ public class LobbyTimer extends TimerTask {
         LobbyHandler lobbyHandler = Telegames.getInstance().getLobbyHandler();
 
         for (Lobby lobby : new ArrayList<>(lobbyHandler.getActiveLobbies().values())) {
+            LogHandler.debug(String.valueOf(currentTime - lobby.getLastLobbyAction()));
             if (currentTime - lobby.getLastLobbyAction() >= 600000) {
                 lobbyHandler.expireLobby(lobby);
             }
