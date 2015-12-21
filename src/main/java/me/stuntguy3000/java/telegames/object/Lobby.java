a5a64bedbf66acc1f5ef213b29160dfbf501aad6
Luke Anderson <stuntguy3000@gmail.com>
2015-12-21 14:23:55 +1030

    Fix issue, typo

--- src/main/java/me/stuntguy3000/java/telegames/object/Lobby.java
+++ src/main/java/me/stuntguy3000/java/telegames/object/Lobby.java
@@ -217,9 +217,7 @@ public class Lobby {
 
             sendMessage(SendableTextMessage.builder().message(TelegramEmoji.JOYSTICK.getText() + " *Starting game: " + newGame.getGameName() + "*").parseMode(ParseMode.MARKDOWN).replyMarkup(new ReplyKeyboardHide()).build());
             String response = newGame.tryStartGame();
-            sendMessage("[DEBUG] RESPONSE NULL? " + (response == null));
-            sendMessage("[DEBUG] RESPONSE: " + (response));
-            if (response != null) {
+            if (response == null) {
                 currentGame = newGame;
                 Telegames.getInstance().getLobbyHandler().startTimer(this);
                 Telegames.getInstance().getConfigHandler().getUserStatistics().addGame(newGame);
